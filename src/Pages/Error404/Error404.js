7e915bdc7a9907329193f9c1f0e18118c2cd990b
sohagislampk <sohagislampk@gmail.com>
2022-10-27 12:52:20 +0600

    Error page added

---
+++ src/Pages/Error404/Error404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1 className='tesxt-3xl text-red-600 text-center mt-24'>Page Not Found ... error 404</h1>
+        </div>
+    );
+};
+
+export default Error404;
--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -4,6 +4,7 @@ import Main from '../../Layouts/Main';
 import Blog from '../../Pages/Blog/Blog';
 import Categories from '../../Pages/Categories/Categories';
 import CourseDetails from '../../Pages/CourseDetails/CourseDetails';
+import Error404 from '../../Pages/Error404/Error404';
 import Home from '../../Pages/Home/Home';
 import Login from '../../Pages/Login/Login';
 import Profile from '../../Pages/Profile/Profile';
@@ -11,45 +12,52 @@ import Register from '../../Pages/Register/Register';
 import CheckOut from '../../Pages/Shared/CheckOut/CheckOut';
 import PrivateRouter from '../PrivateRouter/PrivateRouter';
 
-export const routes = createBrowserRouter([{
-    path: '/',
-    element: <Main></Main>,
-    children: [
-        {
-            path: '/',
-            element: <Home></Home>,
-            loader: () => fetch('https://learning-endless-server-a10.vercel.app/courses')
-        },
-        {
-            path: '/category/:id',
-            element: <Categories></Categories>,
-            loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/category/${params.id}`)
-        },
-        {
-            path: '/courses/:id',
-            element: <CourseDetails></CourseDetails>,
-            loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/courses/${params.id}`)
-        },
-        {
-            path: '/checkout/:id',
-            element: <PrivateRouter><CheckOut></CheckOut></PrivateRouter>,
-            loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/courses/${params.id}`)
-        },
-        {
-            path: '/login',
-            element: <Login></Login>
-        },
-        {
-            path: '/register',
-            element: <Register></Register>
-        },
-        {
-            path: '/profile',
-            element: <PrivateRouter><Profile></Profile></PrivateRouter>
-        },
-        {
-            path: '/blog',
-            element: <Blog></Blog>
-        }
-    ]
-}])
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+                loader: () => fetch('https://learning-endless-server-a10.vercel.app/courses')
+            },
+            {
+                path: '/category/:id',
+                element: <Categories></Categories>,
+                loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/category/${params.id}`)
+            },
+            {
+                path: '/courses/:id',
+                element: <CourseDetails></CourseDetails>,
+                loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/courses/${params.id}`)
+            },
+            {
+                path: '/checkout/:id',
+                element: <PrivateRouter><CheckOut></CheckOut></PrivateRouter>,
+                loader: ({ params }) => fetch(`https://learning-endless-server-a10.vercel.app/courses/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/profile',
+                element: <PrivateRouter><Profile></Profile></PrivateRouter>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
+
+    },
+    {
+        path: '*',
+        element: <Error404></Error404>
+    }
+])
